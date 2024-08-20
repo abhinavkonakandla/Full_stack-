b0c43fdab734532c4bb3429c0aaf4d6192f5ac8a
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-08-20 14:06:01 +0530

    arithmatic,assignment and comparison operators are added

--- javascript/complete notes/myNotes.js
+++ javascript/complete notes/myNotes.js
@@ -5,6 +5,41 @@ y=undefined;         //javascript is a dynamically typed language.
 
 /**/    // this is another way of creating comments.
 
+//Arithmatic operators.
+
+
+let a = 2;
+let b= 3;
+
+console.log("a + b =" , a+b);  
+console.log("a - b= ", a-b);
+console.log("a * b= ", a*b);
+console.log("a / b= ", Math.floor(a/b));   //floor is used to print a number which is less than or equal to the given number.
+console.log("a % b= ", a%b);
+console.log("a ** b= ", a**b);
+a++;
+b--;
+console.log("a++ = ", a);    //unary operators
+console.log("b-- = ", b);
+
+//Assignment operators
+
+let c = 5;
+let d = 6;
+console.log("c+= -> " ,c+=1);
+console.log("c-= -> " ,c-=1);
+console.log("c/= -> " ,c/=1);
+console.log("c*= -> " ,c*=1);
+console.log("c%= -> " ,c%=1);
+console.log("c= -> " ,c+=1);
+
+let first = 5;
+let second = 3;
+console.log("5 == 3", 5== 3);
+console.log("5 != 3", 5!= 3);
+console.log("5 >= 3", 5>= 3);
+console.log("5 <= 3", 5<= 3);
+
 
 
 
