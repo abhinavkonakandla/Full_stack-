7de52bebcfb209922d7cf21ef5449be786d97bb3
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-08-20 13:30:26 +0530

    new branch is created.

--- javascript/complete notes/myNotes.js
+++ javascript/complete notes/myNotes.js
@@ -2,3 +2,24 @@ fullName = "Abhinav kumar";
 console.log(fullName);
 x=null;
 y=undefined;         //javascript is a dynamically typed language.
+
+/**/    // this is another way of creating comments.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
