230386298eced107d142e4019356c1ccf7eecadb
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-03 21:03:14 +0530

    javascript used style tag to change the properties

---
+++ javascript/DOM part-2/script.js
@@ -0,0 +1,9 @@
+/*let head = document.querySelector("h1");
+console.log(head.getAttribute("class"));
+
+let paraG = document.querySelector("p");
+console.log(paraG.setAttribute("class","paragraph"));*/
+
+let upP = document.querySelector("p");
+upP.style.backgroundColor = "blue";
+upP.style.color = "white";
