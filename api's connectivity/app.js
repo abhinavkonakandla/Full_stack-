cf6dade30ba88fa5b4447887009e0638037ab73f
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-14 19:09:42 +0530

    eventlistenet added to the button

--- api's connectivity/app.js
+++ api's connectivity/app.js
@@ -1,15 +1,17 @@
  const URL =  "https://api.thecatapi.com/v1/images/0XYvRd7oD";
 
 const castPara = document.querySelector("#cast");
+const btn = document.querySelector("#btn");
 
-(async ()=>{
+const getCats = async() =>{
     let response = await fetch(URL);
     console.log(response);
     let data = await response.json();
     console.log(data);
     castPara.innerText = data.breeds[0].description;
-})();
+};
 
+btn.addEventListener("click", getCats);
 
 
 
