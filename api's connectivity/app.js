48009b23415e00c298f11daaa7fe84844a227514
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-14 19:32:30 +0530

    promise and async-await approaches tried for the eventlistener

--- api's connectivity/app.js
+++ api's connectivity/app.js
@@ -3,13 +3,26 @@
 const castPara = document.querySelector("#cast");
 const btn = document.querySelector("#btn");
 
-const getCats = async() =>{
+async function getCats(){
     let response = await fetch(URL);
     console.log(response);
     let data = await response.json();
     console.log(data);
     castPara.innerText = data.breeds[0].description;
 };
+
+/*function getCats(){
+    fetch(URL)
+    .then((result)=>{
+        return result.json();
+    })
+    .then((value)=>{
+        castPara.innerText = value.breeds[0].description;
+    })
+}*/
+
+
+
 
 btn.addEventListener("click", getCats);
 
