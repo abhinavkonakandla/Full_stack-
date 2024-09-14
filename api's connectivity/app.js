2f6e5c4ba64a1ee9f5522136295832e703b4a1d7
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-14 19:01:25 +0530

    trying with html elements

--- api's connectivity/app.js
+++ api's connectivity/app.js
@@ -1,11 +1,15 @@
  const URL =  "https://api.thecatapi.com/v1/images/0XYvRd7oD";
 
+const castPara = document.querySelector("#cast");
 
-
-const getCats = async ()=>{
+(async ()=>{
     let response = await fetch(URL);
-    console.log(response.status);
-    let data = response.json();
-    console.log(data)
-}
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+    castPara.innerText = data.breeds[0].description;
+})();
+
+
+
 
