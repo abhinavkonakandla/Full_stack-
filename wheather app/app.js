58609be41f12d7bb6962fe52f9b3b300629b1ecb
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-17 11:07:00 +0530

    city,temp and humidity updated in web page

--- wheather app/app.js
+++ wheather app/app.js
@@ -1,10 +1,16 @@
 const apiKey = "ffe85d3db3dcdf61a516106d734ab21c";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=india";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=germany";
 
-async function checkWheather(){
+(async function checkWheather(){
     const response = await fetch(apiURL + `&appid=${apiKey}`);
     const data = await response.json();
-}
+
+    console.log(data);
+
+    document.querySelector(".city").innerText = data.name;
+    document.querySelector(".temp h1").innerText = data.main.temp;
+    document.querySelector(".humidity").innerText = data.main.humidity;
+})();
 
 
 
