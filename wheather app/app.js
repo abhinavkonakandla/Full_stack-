369ad9bb31428816b5b8093b273c06224059ce20
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-17 11:16:53 +0530

    wind speed updated and coluds img accessed.

--- wheather app/app.js
+++ wheather app/app.js
@@ -1,5 +1,8 @@
 const apiKey = "ffe85d3db3dcdf61a516106d734ab21c";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=germany";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=zocca";
+
+const imgSrc = document.querySelector(".rain-img");
+
 
 (async function checkWheather(){
     const response = await fetch(apiURL + `&appid=${apiKey}`);
@@ -10,6 +13,7 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
     document.querySelector(".city").innerText = data.name;
     document.querySelector(".temp h1").innerText = data.main.temp;
     document.querySelector(".humidity").innerText = data.main.humidity;
+    document.querySelector(".wind").innerText = data.wind.speed;
 })();
 
 
