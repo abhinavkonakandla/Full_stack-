31bce0e32e703b63b54aed0e1e52c872c0d07b5a
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-18 13:48:44 +0530

    i have created react app using vite framework and npm set up completed and added a para to the first.

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>We are learning how to install and run the react project.</p>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 2)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
