6c373ee9ed9896d5d0fe20893dfa8d1851557cbf
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-09-30 11:30:51 +0530

    node modules were installed

--- NodeJS/practice/index.js
+++ NodeJS/practice/index.js
@@ -1,1 +1,8 @@
-console.log("Abhinav");
+/*var getName = require("sillyname");
+var name = getName();
+console.log(name);*/
+import * as superheroes from 'superheroes';
+
+const name = superheroes.random();
+
+console.log(`My superhero name is ${name}`);
