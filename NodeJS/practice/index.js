3c1b6d36a4b8ede2824d707a473d0476e9ca404b
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-10-01 10:18:59 +0530

    preparing node modules notes

--- NodeJS/practice/index.js
+++ NodeJS/practice/index.js
@@ -7,6 +7,16 @@ var hero = require("superheroes");
 const rName = hero.randomSuperhero();
 console.log(`my name is ${rName}`);*/
 
+/*
+There are top 10 modules to learn node as beginner.
+1.HTTP
+2.PATH
+3.FILE SYSTEM
+4.URL
+5.OS
+
+
+*/
 
 
 
